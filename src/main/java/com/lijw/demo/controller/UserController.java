122dbc16cadf0543e85439b6e048dd8a3a29968e
lijw <lijw@jintoufs.com>
2016-08-03 18:25:42 +0800

    文件上传

--- src/main/java/com/lijw/demo/controller/UserController.java
+++ src/main/java/com/lijw/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.lijw.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,5 +17,11 @@ public class UserController {
     @RequestMapping({"/userInfo"})
     public String userInfo(HttpServletRequest request, HttpServletResponse response) {
         return "/user/userinfo";
+    }
+
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+    public String fileUpload(HttpServletRequest request, HttpServletResponse response, MultipartFile file){
+        System.out.println(file.toString());
+        return "/user/fileupload";
     }
 }
